3c04324e82c312278e558b3600fccfc533a5c7fb
sudddenly <tonightfeel@gmail.com>
2018-07-04 23:44:00 +0800

    java秒杀python

---
+++ java/Peak Index in a Mountain Array.java
@@ -0,0 +1,13 @@
+// runtime 3ms
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int l = 0, r = A.length - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (A[mid] < A[mid + 1]) l = mid;
+            else if (A[mid - 1] > A[mid]) r = mid;
+            else return mid;
+        }
+        return 0;
+    }
+}

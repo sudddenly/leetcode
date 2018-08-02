29795a6434f2797f6b7707795e381d2a33adc77f
sudddenly <tonightfeel@gmail.com>
2018-08-02 09:06:42 +0800

    344

---
+++ java/344_reverse-string.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String reverseString(String s) {
+        byte[] bytes = s.getBytes();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            byte temp = bytes[i];
+            bytes[i] = bytes[j];
+            bytes[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(bytes);
+    }
+}

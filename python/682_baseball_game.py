e75db78b576daf20b9f89ad3c6423f43fe580714
sudddenly <tonightfeel@gmail.com>
2018-08-06 09:09:14 +0800

    682

---
+++ python/682_baseball_game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        # Time: O(n)
+        # Space: O(n)
+        history = []
+        for op in ops:
+            if op == 'C':
+                history.pop()
+            elif op == 'D':
+                history.append(history[-1] * 2)
+            elif op == '+':
+                history.append(history[-1] + history[-2])
+            else:
+                history.append(int(op))
+        return sum(history)

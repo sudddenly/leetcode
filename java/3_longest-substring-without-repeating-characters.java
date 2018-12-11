6c638acf2144f8d9ab368fe350c3945959713c76
sudddenly <tonightfeel@gmail.com>
2018-12-12 01:13:10 +0800

    3

---
+++ java/3_longest-substring-without-repeating-characters.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+//      滑动窗口
+//      时间复杂度: O(len(s))
+//      空间复杂度: O(len(charset))
+        int[] freq = new int[256];
+        int l = 0, r = -1;
+        int res = 0;
+        // 整个循环从 l == 0; r == -1 这个空窗口开始
+        // 到l == s.size(); r == s.size()-1 这个空窗口截止
+        // 在每次循环里逐渐改变窗口, 维护freq, 并记录当前窗口中是否找到了一个新的最优值
+        while (l < s.length()) {
+            if (r + 1 < s.length() && freq[s.charAt(r + 1)] == 0) {
+                r++;
+                freq[s.charAt(r)]++;
+            } else {
+                freq[s.charAt(l)]--;
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
